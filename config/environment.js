03257a194847e117eb3a0f52f232d5f21d89bc5a
Jules Costa <julesleighcosta@gmail.com>
2020-02-05 12:02:43 -0600

    Add Mapbox API token to env

--- config/environment.js
+++ config/environment.js
@@ -47,5 +47,7 @@ module.exports = function(environment) {
     // here you can enable a production-specific feature
   }
 
+  ENV.MAPBOX_ACCESS_TOKEN  = 'pk.eyJ1IjoianVsZXNjb3N0YSIsImEiOiJjazY5bTZ0YnAwaGJwM2txZjczaGF6OWt2In0.I3Bf86mL5VKinb6SCyNmVw';
+
   return ENV;
 };
