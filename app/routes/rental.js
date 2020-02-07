e6d07c4d44923534dd8f9fd830e97ec0c597cb45
Jules Costa <julesleighcosta@gmail.com>
2020-02-07 08:56:47 -0600

    Test rental detail component

--- app/routes/rental.js
+++ app/routes/rental.js
@@ -8,7 +8,7 @@ const COMMUNITY_CATEGORIES = [
 
 export default class RentalRoute extends Route {
     async model(params) {
-        let response = await fetch(`/api/rental/${params.rental_id}.json`);
+        let response = await fetch(`/api/rentals/${params.rental_id}.json`);
         let { data } = await response.json();
 
         let { id, attributes } = data;
@@ -20,6 +20,6 @@ export default class RentalRoute extends Route {
             type = 'Standalone';
         }
 
-        return { id, type, attributes }; 
+        return { id, type, ...attributes }; 
     }
 }
