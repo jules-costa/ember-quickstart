ca55ef9f7d9a631ff41254a95c4b19d22cf10d84
Jules Costa <julesleighcosta@gmail.com>
2020-02-10 10:40:33 -0600

    Create ember data models

--- app/routes/rental.js
+++ app/routes/rental.js
@@ -1,52 +1,10 @@
-// import Route from '@ember/routing/route';
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-// const COMMUNITY_CATEGORIES = [
-//     'Condo',
-//     'Townhouse',
-//     'Apartment'
-// ];
+export default class RentalRoute extends Route {
+    @service store;
 
-// export default class RentalRoute extends Route {
-//     async model(params) {
-//         let response = await fetch(`/api/rentals/${params.rental_id}.json`);
-//         let { data } = await response.json();
-
-//         let { id, attributes } = data;
-//         let type;
-
-//         if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
-//             type = 'Community';
-//         } else {
-//             type = 'Standalone';
-//         }
-
-//         return { id, type, ...attributes }; 
-//     }
-// }
-
-import Model, { attr } from '@ember-data/model';
-
-const COMMUNITY_CATEGORIES = [
-    'Condo', 
-    'Townhouse',
-    'Apartment'
-];
-
-export default class RentalModel extends Model {
-    @attr title;
-    @attr owner;
-    @attr city;
-    @attr location;
-    @attr category;
-    @attr image;
-    @attr bedrooms;
-    @attr description;
-
-    get type() {
-        if (COMMUNITY_CATEGORIES.includes(this.category)) {
-            return 'Community';
-        } else {
-            return 'Standalone';
-        }
+    async model(params) {     
+        return this.store.find('rental', params.rental_id);
     }
 }
